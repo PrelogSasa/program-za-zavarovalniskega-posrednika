f8733c94aa042494f07e7c06f1e9e4ba4ee93695
Saša <sasa.prelog6@gmail.com>
2018-05-28 12:54:07 +0200

    2. osnutek

--- glavna_datoteka.py
+++ glavna_datoteka.py
@@ -7,10 +7,10 @@ class Stranka:
         self.priimek = priimek
         self.datum_rojstva = datum_rojstva
 
-    def Nastavi_starost(self,datum_rojstva=self.datum_rojstva, današnji_datum=DANAŠNJI_DATUM):
+    #def Nastavi_starost(self,datum_rojstva=self.datum_rojstva, današnji_datum=DANAŠNJI_DATUM):
         #zračuni starost
         #preveri če čez 18 - če ni error
-        self.starost =
+        #self.starost =
 
     def _repr_(self):
         return 'Stranka({}, {}, {})'.format(self.ime, self.priimek, self.datum_rojstva)
@@ -20,23 +20,49 @@ class Stranka:
 
 
 class Nezgodno_zavarovanje:
-    def _init_(self, trajanje):
+    def _init_(self, trajanje, paket):
         self.trajanje = trajanje
-        
-    def Nastavi_lastnika(self, stranka):
-        self.lastnik = stranka
+        self.paket = paket
+        with open('številka_zavarovanja.txt') as dat:
+            številka_zavarovanja = dat.readline()
+        self.številka = številka_zavarovanja
+        with open('številka_zavarovanja.txt', 'w') as dat:
+            številka_zavarovanja_večja = int(self.številka) + 1
+            številka_zavarovanja_string = str(številka_zavarovanja_večja).zfill(6)
+            dat.write(številka_zavarovanja_string)
 
-    def Nastavi_premijo(self, interval_plačevanja, trajanje = self.trajanje, starost=self.lastnik.starost):
-        #zračuni premijo glede na starost in interval plačevanja
-        #pokaži kok bi mogl plačvat
-
-    def Ustvari_zavarovanje(self):
-        #ustvari oz dodaj v datoteko zavarovanje
-        #dodaj lastnika v bazo oseb (če še ni)
-
-        
     def _str_(self):
         return 'Nezgodno zavarovanje št. {}, lastnik: {}'.format(self.številka, self.lastnik)
 
     def _repr_(self):
-        return 'Nezgodno_zavarovanje({})'.format(self.trajanje)
+        return 'Nezgodno_zavarovanje({}, {})'.format(self.trajanje, self.paket)
+        
+    def Nastavi_lastnika(self, stranka):
+        self.lastnik = stranka 
+
+    def Nastavi_premijo(self, interval_plačevanja, trajanje=self.trajanje, starost=self.lastnik.starost, paket=self.paket):
+        trajanje = {1: 1, 5: 0.95, 10: 0.85}
+        paketi = {'velik': 200, 'majhen': 150}
+        if starost >= 65:
+            k = 1.2
+        else:
+            k = 1
+        premija = trajanje.get(trajanje) * k * paketi.get(paket) * trajanje
+        self.premija = premija
+        if interval_plačevanja == 'mesečno':
+            plačilo = premija / trajanje / 12
+            return 'Vaše mesečno plačilo je {}'.format(plačilo)
+        else:
+            plačilo = premija / trajanje
+            return 'Vaše letno plačilo je {}'.format(plačilo)
+
+    #def Ustvari_zavarovanje(self):
+        #ustvari oz dodaj v datoteko zavarovanje
+        #dodaj lastnika v bazo oseb (če še ni)
+
+        
+    
+
+#def dodaj_stranko_na_seznam(stranka):
+    
+    
