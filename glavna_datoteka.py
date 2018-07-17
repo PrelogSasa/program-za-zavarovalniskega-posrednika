56ad5497991790cfc672dd91091304fc4a9cce22
Saša Prelog <sasa.prelog6@gmail.com>
2018-07-17 19:27:01 +0200

    popravki in del funkcije ustvari_zavarovanje

--- glavna_datoteka.py
+++ glavna_datoteka.py
@@ -2,25 +2,20 @@ DANAŠNJI_DATUM = '5.5.2017'
 
 
 class Stranka:
-    def _init_(self, ime, priimek, datum_rojstva):
+    def __init__(self, ime, priimek, starost):
         self.ime = ime
         self.priimek = priimek
-        self.datum_rojstva = datum_rojstva
+        self.starost = starost
 
-    #def Nastavi_starost(self,datum_rojstva=self.datum_rojstva, današnji_datum=DANAŠNJI_DATUM):
-        #zračuni starost
-        #preveri če čez 18 - če ni error
-        #self.starost =
+    def __repr__(self):
+        return 'Stranka({}, {}, {})'.format(self.ime, self.priimek, self.starost)
 
-    def _repr_(self):
-        return 'Stranka({}, {}, {})'.format(self.ime, self.priimek, self.datum_rojstva)
-
-    def _str_(self):
-        return '{} {}, datum rojstva: {}'.format(self.ime, self.priimek, self.datum_rojstva)
+    def __str__(self):
+        return '{} {}, starost: {}'.format(self.ime, self.priimek, self.starost)
 
 
 class Nezgodno_zavarovanje:
-    def _init_(self, trajanje, paket):
+    def __init__(self, trajanje, paket):
         self.trajanje = trajanje
         self.paket = paket
         with open('številka_zavarovanja.txt') as dat:
@@ -32,38 +27,56 @@ class Nezgodno_zavarovanje:
             dat.write(številka_zavarovanja_string)
             
 
-    def _str_(self):
+    def __str__(self):
         return 'Nezgodno zavarovanje št. {}, lastnik: {}'.format(self.številka, self.lastnik)
 
-    def _repr_(self):
+    def __repr__(self):
         return 'Nezgodno_zavarovanje({}, {})'.format(self.trajanje, self.paket)
         
-    def Nastavi_lastnika(self, stranka):
-        self.lastnik = stranka 
-
-    def Nastavi_premijo(self, interval_plačevanja, trajanje=self.trajanje, starost=self.lastnik.starost, paket=self.paket):
-        trajanje = {1: 1, 5: 0.95, 10: 0.85}
-        paketi = {'velik': 200, 'majhen': 150}
-        if starost >= 65:
+    def nastavi_lastnika(self, stranka):
+        self.lastnik = stranka
+    
+    def nastavi_premijo(self, interval_plačevanja):
+        if self.lastnik.starost >= 65:
             k = 1.2
         else:
             k = 1
-        premija = trajanje.get(trajanje) * k * paketi.get(paket) * trajanje
-        self.premija = premija
+        trajanja = {1: 1, 5: 0.95, 10: 0.85}
+        paketi = {'velik': 200, 'majhen': 150}
+        self.premija = trajanja.get(self.trajanje) * k * paketi.get(self.paket) * self.trajanje
         if interval_plačevanja == 'mesečno':
-            plačilo = premija / trajanje / 12
+            plačilo = self.premija / self.trajanje / 12
             return 'Vaše mesečno plačilo je {}'.format(plačilo)
         else:
-            plačilo = premija / trajanje
+            plačilo = self.premija / self.trajanje
             return 'Vaše letno plačilo je {}'.format(plačilo)
 
-    #def Ustvari_zavarovanje(self):
+    def ustvari_zavarovanje(self):
         #ustvari oz dodaj v datoteko zavarovanje
-        #dodaj lastnika v bazo oseb (če še ni)
+        with open('baza_oseb.txt', 'a') as baza_oseb:
+            pass
+        with open('baza_oseb.txt') as baza_oseb:
+            osebe = baza_oseb.readlines()
+        lastnika_ni_v_bazi = True
+        for i in range(len(osebe)):
+            if osebe[i].startswith('{}, {}, {}'.format(self.lastnik.ime, self.lastnik.priimek, self.lastnik.starost)):
+                osebe[i] = osebe[i].strip() + ', {}\n'.format(self.številka)
+                lastnika_ni_v_bazi = False
+                break
+        if lastnika_ni_v_bazi:
+            osebe += ['{}, {}, {}: {}'.format(self.lastnik.ime, self.lastnik.priimek, self.lastnik.starost, self.številka)]
+        with open('baza_oseb.txt', 'w') as baza_oseb:
+            for vrstica in osebe:
+                baza_oseb.write(vrstica)
 
         
-    
-
-#def dodaj_stranko_na_seznam(stranka):
-    
-    
+##Domen = Stranka('Maja', 'Kos', 19)
+##Zavarovanje2= Nezgodno_zavarovanje(10, 'velik')
+##Zavarovanje2.nastavi_lastnika(Domen)
+##Zavarovanje2.nastavi_premijo('letno')
+##Zavarovanje2.ustvari_zavarovanje()
+##Domen = Stranka('Kaj', 'Delas', 40)
+##Zavarovanje2= Nezgodno_zavarovanje(10, 'velik')
+##Zavarovanje2.nastavi_lastnika(Domen)
+##Zavarovanje2.nastavi_premijo('letno')
+##Zavarovanje2.ustvari_zavarovanje()
